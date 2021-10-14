67df6e61bbd72570d9203ce36a65f4d666c2148f
John Mai <Maijohn960@gmail.com>
2021-10-14 11:32:18 -0400

    first commit

---
+++ HeroAndVillains.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeroAndVillains.WebMVC.MvcApplication" Language="C#" %>
---
+++ HeroAndVillains.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HeroAndVillains.WebMVC.Startup))]
+namespace HeroAndVillains.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
